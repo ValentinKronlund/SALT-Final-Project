701ff8e9e76eb74c7b09a97c92765a6b359e46cb
Rhodox <70885671+Rhodox@users.noreply.github.com>
2020-12-01 14:07:33 +0100

    fixed bug in schedule-planner page
    INFO: the first push was overwritten with old data, this is a new push
    
    schedule-planner page not working correctly with other code changes.
    
    added some async code to createActivity.js that updates local storage as that is what we're using now

--- client/src/components/SchedulePlanner/CreateActivity.js
+++ client/src/components/SchedulePlanner/CreateActivity.js
@@ -1,78 +1,89 @@
 import React, { useState, useContext } from "react";
 import { ActivitiesContext, UpdateAtivitiesContext } from "../App";
-import Context from "../../contexts/UserContext";
+import userContext from "../../contexts/UserContext";
 
 import "../../styles/createActivity.css";
 
+import updateUser from "../../hooks/updateUser";
+
 export default function CreateActivity({ toggleIsHidden }) {
-	const updateActivitiesArray = useContext(UpdateAtivitiesContext);
-	const ActivitiesArray = useContext(ActivitiesContext);
-	const user = useContext(Context);
+  const updateActivitiesArray = useContext(UpdateAtivitiesContext);
+  const ActivitiesArray = useContext(ActivitiesContext);
+  const user = useContext(userContext);
 
-	const [Description, updateDescription] = useState("");
-	const [Activity, updateActivity] = useState("");
-	const [Time, updateTime] = useState("");
+  const [Description, updateDescription] = useState("");
+  const [Activity, updateActivity] = useState("");
+  const [Time, updateTime] = useState("");
 
-	const addActivity = () => {
-		const fetchUrl = `http://localhost:1337/api/mongoDB/?firstName=${user.userInfo.firstName}`;
-		const newArray = ActivitiesArray;
+  const addActivity = () => {
+    const fetchUrl = `http://localhost:1337/api/mongoDB/?firstName=${user.userInfo.firstName}`;
 
-		const newActivity = {
-			Activity: Activity,
-			Description: Description,
-			Time: Time,
-		};
+    const newActivity = {
+      Activity,
+      Description,
+      Time,
+    };
 
-		const requestOptions = {
-			method: "PUT",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({ schedule: newArray }),
-		};
+    const newArray = ActivitiesArray;
+    newArray.push(newActivity);
+    updateActivitiesArray(newArray);
 
-		newArray.push(newActivity);
-		updateActivitiesArray(newArray);
+    const requestOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ schedule: newArray }),
+    };
 
-		fetch(fetchUrl, requestOptions);
-		toggleIsHidden(false);
-	};
+    fetch(fetchUrl, requestOptions)
+      .then(async () => {
+        const fetchUser = await updateUser();
+        console.log('--- fetchUser ---\n', fetchUser);
+        user.setUserInfo(fetchUser);
+        toggleIsHidden(true);
+      });
+  };
 
-	return (
-		<>
-			<input
-				type="text"
-				className="input-field"
-				name="activity"
-				placeholder="Add an activity"
-				value={Activity}
-				autoComplete="off"
-				onChange={(e) => updateActivity(e.currentTarget.value)}
-				required
-			/>
-			<hr className="top-input-underline"></hr>
-			<input
-				type="text"
-				className="input-field"
-				name="description"
-				placeholder="Add a description"
-				value={Description}
-				autoComplete="off"
-				onChange={(e) => updateDescription(e.currentTarget.value)}
-				required
-			/>
-			<br></br>
-			<input
-				type="time"
-				className="input-field"
-				name="time"
-				placeholder="Time"
-				value={Time}
-				onChange={(e) => updateTime(e.currentTarget.value)}
-				required
-			/>
-			<br></br>
-			<button className="add-window-button add-act" onClick={addActivity} type="submit">
-				<p>Add Activity</p>
-			</button>
-		</>
-	);
+  return (
+    <>
+      <input
+        type="text"
+        className="input-field"
+        name="activity"
+        placeholder="Add an activity"
+        value={Activity}
+        autoComplete="off"
+        onChange={(e) => updateActivity(e.currentTarget.value)}
+        required
+      />
+      <hr className="top-input-underline"></hr>
+      <input
+        type="text"
+        className="input-field"
+        name="description"
+        placeholder="Add a description"
+        value={Description}
+        autoComplete="off"
+        onChange={(e) => updateDescription(e.currentTarget.value)}
+        required
+      />
+      <br></br>
+      <input
+        type="time"
+        className="input-field"
+        name="time"
+        placeholder="Time"
+        value={Time}
+        onChange={(e) => updateTime(e.currentTarget.value)}
+        required
+      />
+      <br></br>
+      <button
+        className="add-window-button add-act"
+        onClick={addActivity}
+        type="submit"
+      >
+        <p>Add Activity</p>
+      </button>
+    </>
+  );
 }
