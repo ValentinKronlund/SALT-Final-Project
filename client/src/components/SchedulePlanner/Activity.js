c793d9ac8dce31a33d1f016f9a36127d4b235ecd
Rhodox <70885671+Rhodox@users.noreply.github.com>
2020-12-01 18:09:30 +0100

    Added functionality to date input on schedule-planner page
    The date input in the schedule-planner page now correctly adds, and shows, the date of the schedule item in our database and website.
    
    Also spiced up the schedule-planner  layout a bit more

--- client/src/components/SchedulePlanner/Activity.js
+++ client/src/components/SchedulePlanner/Activity.js
@@ -5,10 +5,20 @@ import Context from "../../contexts/UserContext";
 //import "../../styles/activity.css";
 import "../../styles/schedulePlanner.css";
 
-export default function Activity({ Activity, Description, Time, deleted, updateDeleted }) {
+export default function Activity({ Activity, Description, Time, userDate, deleted, updateDeleted }) {
 	const ActivitiesArray = useContext(ActivitiesContext);
 	const updateActivitiesArray = useContext(UpdateAtivitiesContext);
 	const user = useContext(Context);
+
+	const formatDate = () => {
+		let d = new Date(userDate);
+		const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
+		const mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(d);
+		const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
+		const formattedDate = `${da}-${mo}-${ye}`;
+		
+		return (<p>{formattedDate}</p>);
+	}
 
 	const removeActivity = () => {
 		const newArray = ActivitiesArray;
@@ -39,25 +49,27 @@ export default function Activity({ Activity, Description, Time, deleted, updateD
 	};
 
 	return (
-		<div className="activity-container">
+		<div className="activity-label">
+			<p className="activity-date">{userDate ? formatDate() : null}</p>
+			<div className="activity-container">
+				<div className="time-text">
+					<p>{Time}</p>
+				</div>
 
-			<div className="time-text">
-				<p>{Time}</p>
-			</div>
+				<div className="activity-text">
+					<p>{Activity}</p>
+				</div>
 
-			<div className="activity-text">
-				<p>{Activity}</p>
-			</div>
+				<div className="description-text">
+					<p>{Description}</p>
+				</div>
 
-			<div className="description-text">
-				<p>{Description}</p>
-			</div>
-
-			<div className="button-container">
-			<button className="buttons">Mark as done</button>
-			<button className="buttons" onClick={removeActivity}>
-				Delete
-			</button>
+				<div className="button-container">
+				<button className="buttons">Mark as done</button>
+				<button className="buttons" onClick={removeActivity}>
+					Delete
+				</button>
+				</div>
 			</div>
 		</div>
 	);
